749e3ad9d55456a7cf3ce68b7c7fe9536e92cdab
Rafal <pawelec.rafal@gmail.com>
2019-10-30 21:28:47 +0100

    first commit

---
+++ precache-manifest.73a0e322fc4242518a529b1158763261.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521410300366c48283778a3a046b1d18",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ad69e13039299140c3b0",
+    "url": "./static/css/main.5561bb22.chunk.css"
+  },
+  {
+    "revision": "a1a25b510738cb05c55a",
+    "url": "./static/js/2.fd30c612.chunk.js"
+  },
+  {
+    "revision": "ad69e13039299140c3b0",
+    "url": "./static/js/main.57ab6611.chunk.js"
+  },
+  {
+    "revision": "bdb5a71155ab1aaf3e90",
+    "url": "./static/js/runtime-main.0e1cea8a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
